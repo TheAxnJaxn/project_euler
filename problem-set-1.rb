24f91ddb9969dd06eeb557be7d61ff142b251b74
Krista Jackson <krista.lynn.jackson@gmail.com>
2015-08-06 21:11:08 -0700

    Add answer for problem 4.

--- problem-set-1.rb
+++ problem-set-1.rb
@@ -40,3 +40,54 @@ end
 
 prob2_num = 4_000_000
 puts "2. The sum of even-valued Fibonacci terms below #{prob2_num} is: #{problem_2(prob2_num)}"
+
+# Problem 3 - Largest Prime Factor ####################
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143 ?
+
+
+def problem_3(num)
+  # create prime factors from a minimum of the square root of num
+  # to a max of num/2 (because that's the largest potential prime),
+  # and then iterate through that collection of primes from largest end.
+
+  # starting_num = sqrt(num).to_i
+  # ending_num = num/2
+end
+
+# write a helper method that checks is_prime? from 2 up to the square root
+# of the number (which is the maximum possible divider where %% divider could == 0)
+
+prob3_num = 600_851_475_143
+puts "3. The largest prime factor of the number #{prob3_num} is: #{problem_3(prob3_num)}"
+
+# Problem 4 - Largest Palindrome Factor ###############
+# A palindromic number reads the same both ways.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+def problem_4
+  num1 = 999
+  largest_pal = 0
+
+  while num1 > 99
+    num2 = num1 - 1
+    while num2 > 99
+      product = num1 * num2
+      if is_palindrome?(product) && product > largest_pal
+        largest_pal = product
+      end
+      num2 -= 1
+    end
+    num1 -= 1
+  end
+
+  return largest_pal
+end
+
+def is_palindrome?(num)
+  str = num.to_s
+  return str == str.reverse
+end
+
+puts "4. The largest palindromic product of two 3-digit numbers is: #{problem_4}"
